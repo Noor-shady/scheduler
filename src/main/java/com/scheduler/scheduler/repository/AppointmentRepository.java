40db4442dddaacb2f624c31eeaf8c4144ffb1083
noor <c3074048@newcastle.ac.uk>
2025-12-08 17:34:43 +0000

    deleted

--- src/main/java/com/scheduler/scheduler/repository/AppointmentRepository.java
+++ src/main/java/com/scheduler/scheduler/repository/AppointmentRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     // .save(), .findAll(), .delete(), .findById()
-    // I will add custom queries later like this:
     // List<Appointment> findByCustomerEmail(String email);
 }
 
